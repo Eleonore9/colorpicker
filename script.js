1039558e5ab7bcdd897aaad9a3b46369257776df
Eleonore9 <eleonore.mayola@ymail.com>
2014-01-26 23:20:41 +0100

    can add color to favorite

--- script.js
+++ script.js
@@ -3,10 +3,23 @@ $(document).ready(function() {
 	$(document).on('keydown keyup keypress', '#color', function() {
 		$color = $('#color').val();
 		setPreviewColor($color);
-	});	
+		//$('#color').val("");
+		//$("#add-to-favorite").on('click', function() {
+			//addBox($color);
+		//});
+	});
+		$("#add-to-favorite").on('click', function() {
+			addBox($color);
+		});
+
 });
 
 function setPreviewColor(color) {
 	$(".preview").css('background-color', color);
 	$(".color-code").css('background-color', color);
+}
+
+function addBox(color) {
+	console.log(color);
+	$("#colors").prepend("<div class='item' style='background-color: " + color + ";'></div>");
 }
